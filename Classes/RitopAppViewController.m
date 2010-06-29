4f14f901f3a9d9918ecc5639ce5346b2a6799247
Toni Suter <tonisuter@me.com>
2010-06-30 00:36:24 +0200

    added some test controls

--- Classes/RitopAppViewController.m
+++ Classes/RitopAppViewController.m
@@ -22,13 +22,25 @@
 */
 
 - (void)loadView {
-	UIViewController *myViewController1 = [[UIViewController alloc] init];
-	UINavigationController *myNavigationController1 = [[UINavigationController alloc] initWithRootViewController:myViewController1];
+	UITableViewController *myTableViewController1 = [[UITableViewController alloc] initWithStyle:UITableViewStylePlain];
+	
+	[myTableViewController1 setTitle:@"NavigationController 1"];
+	UINavigationController *myNavigationController1 = [[UINavigationController alloc] initWithRootViewController:myTableViewController1];
 	[myNavigationController1 setTitle:@"Page 1"];
+	[myNavigationController1 setToolbarHidden:NO];
+	UIBarButtonItem *addButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAdd 
+																				 target:self 
+																				 action:@selector(addSomething:)];
+	[myTableViewController1 setToolbarItems:[NSArray arrayWithObjects:addButton, nil] animated:YES];
+	 
+	
 	UIViewController *myViewController2 = [[UIViewController alloc] init];
+	[myViewController2 setTitle:@"NavigationController 2"];
 	UINavigationController *myNavigationController2 = [[UINavigationController alloc] initWithRootViewController:myViewController2];
 	[myNavigationController2 setTitle:@"Page 2"];
+	
 	UIViewController *myViewController3 = [[UIViewController alloc] init];
+	[myViewController3 setTitle:@"NavigationController 3"];
 	UINavigationController *myNavigationController3 = [[UINavigationController alloc] initWithRootViewController:myViewController3];
 	[myNavigationController3 setTitle:@"Page 3"];
 	
@@ -36,16 +48,23 @@
 	[myTabBarController setViewControllers:[NSArray arrayWithObjects:myNavigationController1, myNavigationController2, myNavigationController3, nil]];
 	
 	self.view = myTabBarController.view;
+	
+//	UIViewController *myContentViewController = [[UIViewController alloc] init];
+//	UIPopoverController *myPopoverController = [[UIPopoverController alloc] initWithContentViewController:myContentViewController];
+//	[myPopoverController presentPopoverFromBarButtonItem:selectItem permittedArrowDirections:UIPopoverArrowDirectionAny animated:YES];
 }
 
+- (void)addSomething:(id)sender
+{
+	UIAlertView *myAlertView = [[UIAlertView alloc] initWithTitle:@"Add Something" 
+														  message:@"This is a test message." 
+														 delegate:self 
+												cancelButtonTitle:nil 
+												otherButtonTitles:@"OK", nil];
+	[myAlertView show];
+} 
 
 
-/*
-// Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
-- (void)viewDidLoad {
-    [super viewDidLoad];
-}
-*/
 
 
 // Override to allow orientations other than the default portrait orientation.
