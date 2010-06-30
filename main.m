9dd791bbe81bb650f8ce64b78ee4830f47cbe9c9
Simeon Haefliger <simeonhaefliger@My-Mac-Pro.local>
2010-06-30 21:27:25 +0200

    simis erste erfahrungne mit git

--- main.m
+++ main.m
@@ -3,7 +3,7 @@
 //  RitopApp
 //
 //  Created by Toni Suter on 29.06.10.
-//  Copyright __MyCompanyName__ 2010. All rights reserved.
+//  Copyright !!! mer bruched en geilere name !!! 2010. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
